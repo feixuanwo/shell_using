e588596101be2c3aec15c9b4b70f26d4a5e0f5f6
redhat <redhat@localhost.localdomain>
2017-01-17 21:30:00 -0800

    update

--- FileTaskUtils_check.sh
+++ FileTaskUtils_check.sh
@@ -7,6 +7,15 @@
 
 . ~/.bash_profile
 
+#判断参数是否正确,如不正确则提示参数信息并退出
+if [ "$1" != "start" ] ; then
+    echo 
+    echo "=====参数输入有误，请加上参数“start”来运行脚本====="
+    echo 
+    exit 1
+fi
+
+#定义常量
 LOG_FILE="/home/qtservices/logs/FileTaskUtils_check.log"      #日志文件，记录日志输出的信息
 SHELL_PATH="/home/qtservices/shell/normal/"                   #脚本路径
 LOCK_NAME=".FileTaskUtils_lock"                               #锁文件
@@ -49,82 +58,75 @@ releaseLock()
     rm ${SHELL_PATH}${LOCK_NAME}
 }
 
-#脚本需要添加参数start来运行
-if [ "$1" == "start" ] ; then
-    #判断脚本是否已被加锁
-    if [ -f ${SHELL_PATH}${LOCK_NAME} ] ; then
-        echo "This shell has been locked"
-        exit 1
-    else
-        getLock
-    fi
-    
-    #启动服务
-    if [ "$DATE_TIME" == "$START_TIME" ] ; then 
-        #检测服务进程是否存在，不存在，启动服务
-        check
-        if [ "$?" == "1" ] ; then
-            sh /home/qtservices/FileTaskUtils/startFileTaskServer.sh  
-            log "1" "FileTaskUtils 服务未启动，已执行启动程序启动服务"
-        fi
-        sleep 5
-        #再次检测服务是否启动成功
-        check
-        if [ "$?" == "0" ] ; then 
-            log "0" "FileTaskUtils 服务已启动" 
-        else 
-            log "2" "FileTaskUtils 服务启动失败" 
-            echo "FileTaskUtils 服务启动失败" | mail -s ERROR shiye@imobpay.com
-        fi
-    fi
-    
-    ##启动服务
-    #if [ "$DATE_TIME" -lt "$END_TIME" ] ; then 
-    #    #检测服务进程是否存在，不存在，启动服务
-    #    check
-    #    if [ "$?" == "1" ] ; then
-    #        sh /home/qtservices/FileTaskUtils/startFileTaskServer.sh  
-    #        log "1" "FileTaskUtils 服务未启动，已执行启动程序启动服务"
-    #    fi
-    #    sleep 5
-    #    #再次检测服务是否启动成功
-    #    check
-    #    if [ "$?" == "0" ] ; then 
-    #        log "0" "FileTaskUtils 服务已启动" 
-    #    else 
-    #        log "2" "FileTaskUtils 服务启动失败" 
-    #        echo "FileTaskUtils 服务启动失败" | mail -s ERROR shiye@imobpay.com
-    #    fi
-    #fi
-    
-    #关闭服务
-    if [ "$DATE_TIME" == "$END_TIME" ] ; then
-        #输出日志
-        log "0" "到达结束时间END_TIME:$END_TIME，准备关闭FileTaskUtils服务"
-        #运行关闭脚本，关闭服务  
-        sh /home/qtservices/FileTaskUtils/stopFileTaskServer.sh
-        sleep 5
-        #再次检测服务是否关闭，如未成功关闭，则采用杀进程关闭服务  
-        check
-        if [ "$?" == "0" ] ; then 
-            log "1" "服务未正常关闭，将使用杀死进程方式关闭服务" 
-            kill -9 `ps -ef | grep FileTaskUtils.jar | grep -v grep | awk '{print $2}'` 
-        fi
-        #再次检测服务，正常关闭，输出日志;未正常关闭，发送邮件报警  
-        sleep 5
-        check
-        if [ "$?" == "1" ] ; then
-            log "0" "FileTaskUtils 服务已关闭" 
-        else
-            log "2" "FileTaskUtils 服务未正常关闭，请手动关闭" 
-            echo "FileTaskUtils 服务未正常关闭，请手动关闭" | mail -s ERROR shiye@imobpay.com
-        fi
-    fi
-    
-    #解锁
-    releaseLock
+#判断脚本是否已被加锁
+if [ -f ${SHELL_PATH}${LOCK_NAME} ] ; then
+    echo "This shell has been locked"
+    exit 1
 else
-    echo
-    echo "========脚本需要添加参数“start”才可以运行========="
-    echo
+    getLock
 fi
+
+#启动服务
+if [ "$DATE_TIME" == "$START_TIME" ] ; then 
+    #检测服务进程是否存在，不存在，启动服务
+    check
+    if [ "$?" == "1" ] ; then
+        sh /home/qtservices/FileTaskUtils/startFileTaskServer.sh  
+        log "1" "FileTaskUtils 服务未启动，已执行启动程序启动服务"
+    fi
+    sleep 5
+    #再次检测服务是否启动成功
+    check
+    if [ "$?" == "0" ] ; then 
+        log "0" "FileTaskUtils 服务已启动" 
+    else 
+        log "2" "FileTaskUtils 服务启动失败" 
+        echo "FileTaskUtils 服务启动失败" | mail -s ERROR shiye@imobpay.com
+    fi
+fi
+
+##启动服务
+#if [ "$DATE_TIME" -lt "$END_TIME" ] ; then 
+#    #检测服务进程是否存在，不存在，启动服务
+#    check
+#    if [ "$?" == "1" ] ; then
+#        sh /home/qtservices/FileTaskUtils/startFileTaskServer.sh  
+#        log "1" "FileTaskUtils 服务未启动，已执行启动程序启动服务"
+#    fi
+#    sleep 5
+#    #再次检测服务是否启动成功
+#    check
+#    if [ "$?" == "0" ] ; then 
+#        log "0" "FileTaskUtils 服务已启动" 
+#    else 
+#        log "2" "FileTaskUtils 服务启动失败" 
+#        echo "FileTaskUtils 服务启动失败" | mail -s ERROR shiye@imobpay.com
+#    fi
+#fi
+
+#关闭服务
+if [ "$DATE_TIME" == "$END_TIME" ] ; then
+    #输出日志
+    log "0" "到达结束时间END_TIME:$END_TIME，准备关闭FileTaskUtils服务"
+    #运行关闭脚本，关闭服务  
+    sh /home/qtservices/FileTaskUtils/stopFileTaskServer.sh
+    sleep 5
+    #再次检测服务是否关闭，如未成功关闭，则采用杀进程关闭服务  
+    check
+    if [ "$?" == "0" ] ; then 
+        log "1" "服务未正常关闭，将使用杀死进程方式关闭服务" 
+        kill -9 `ps -ef | grep FileTaskUtils.jar | grep -v grep | awk '{print $2}'` 
+    fi
+    #再次检测服务，正常关闭，输出日志;未正常关闭，发送邮件报警  
+    sleep 5
+    check
+    if [ "$?" == "1" ] ; then
+        log "0" "FileTaskUtils 服务已关闭" 
+    else
+        log "2" "FileTaskUtils 服务未正常关闭，请手动关闭" 
+        echo "FileTaskUtils 服务未正常关闭，请手动关闭" | mail -s ERROR shiye@imobpay.com
+    fi
+fi
+
+#解锁
+releaseLock
